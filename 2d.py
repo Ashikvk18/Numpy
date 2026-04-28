d779582dc456087b33f17b968b60e5dde141f715
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-28 04:46:47 -0500

    2d array in nump

---
+++ 2d.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr_2d = np.array([[1, 2, 3], 
+[4, 5, 6]],
+)
+print(arr_2d)
+print(arr_2d.ndim)
+print(arr_2d.shape)
+print(arr_2d.size)
+print(arr_2d.dtype)
+print(arr_2d.itemsize)
+print(arr_2d.nbytes)
