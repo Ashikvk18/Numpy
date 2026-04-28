842e9e01e4d07a5a40090a6c38c75b2f81b9b59f
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-28 04:23:42 -0500

    1d array in np

---
+++ 1d.py
@@ -0,0 +1,9 @@
+import numpy as np
+ar_1d = np.array([1, 2, 3, 4, 5])
+print(ar_1d)
+print(ar_1d.ndim)
+print(ar_1d.shape)
+print(ar_1d.size)
+print(ar_1d.dtype)
+print(ar_1d.itemsize)
+print(ar_1d.nbytes)
