0726c17abbdb06364e670195771a2102dc2ef278
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-04-28 04:48:06 -0500

    multi

--- muliti.py
+++ muliti.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(matrix)
+print(matrix.ndim)
+print(matrix.shape)
+print(matrix.size)
+print(matrix.dtype)
+print(matrix.itemsize)
+print(matrix.nbytes)
