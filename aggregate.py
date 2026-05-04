19f76a1747241946c027b05a148c3f87bf46b695
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-05-04 01:11:19 -0500

    aggregate

---
+++ aggregate.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+print("Sum:", np.sum(arr))
+print("Mean:", np.mean(arr))
+print("Standard deviation:", np.std(arr))
+print("Variance:", np.var(arr))
+print("Minimum:", np.min(arr))
+print("Maximum:", np.max(arr))
+print("Median:", np.median(arr))
