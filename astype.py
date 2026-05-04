d24738f4f8ae16a765547f66ee188e3e42ca5794
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-05-04 01:08:32 -0500

    astype

--- astype.py
+++ astype.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 arr = np.array([1.1, 2.2, 3.3])
+print("Original array data type:", arr.dtype)
 newarr = arr.astype('i')
-print(newarr)
-print(newarr.dtype)
+print("Original array:", arr)
+print("New array:", newarr)
+print("Data type of new array:", newarr.dtype)
