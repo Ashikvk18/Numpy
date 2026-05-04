64177ca58644c4cc186ded7dea299b72f2b1b695
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-05-04 13:50:48 -0500

    diff stack

--- stack.py
+++ stack.py
@@ -1,1 +1,8 @@
 import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+print(np.stack([arr1, arr2], axis=1))
+print("----------------------------")
+print(np.stack([arr1, arr2], axis=0))
