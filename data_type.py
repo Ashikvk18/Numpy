aba605c1ddfb5aecb09176dfca8f593af3ea7788
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-05-04 00:47:12 -0500

    data type

---
+++ data_type.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, ])
+print(arr.dtype) # dtype gives the data type of the array
+
+arr2 = np.array([1, 2, 3, 4], dtype='i4')
+print(arr2.dtype) # dtype gives the data type of the array
