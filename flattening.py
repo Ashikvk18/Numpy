d2bd9d883a41e943d5da6734b863818ff1b44fb6
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-05-04 04:13:55 -0500

    ravel vs copy

--- flattening.py
+++ flattening.py
@@ -5,3 +5,13 @@ print(arr)
 
 # Flattening
 print(arr.flatten()) # it means 1D array
+
+# ravel
+print(arr.ravel()) # it means 1D array
+
+# difference between flatten and ravel
+# flatten returns a copy of the array
+# ravel returns a view of the array
+
+# difference between copy and view: copy can be modified without affecting the original array 
+# view is a reference to the original array so it will affect the original array
