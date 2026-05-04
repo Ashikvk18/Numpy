1496397956e737df73b72b6848120f50f843f52b
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-05-04 00:45:51 -0500

    dimensions

---
+++ ndim.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr_1d = np.array([1, 2, 3])
+arr_2d = np.array([[1, 2, 3], [4, 5, 6]])
+arr_3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+print(arr_1d.ndim) # ndim gives the number of dimensions of the array
+print(arr_2d.ndim) # ndim gives the number of dimensions of the array
+print(arr_3d.ndim) # ndim gives the number of dimensions of the array
