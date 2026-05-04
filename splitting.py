7189dbc7655096696d88863be42865bb4c268667
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-05-04 14:01:16 -0500

    splitting

--- splitting.py
+++ splitting.py
@@ -3,3 +3,9 @@ import numpy as np
 # equally split
 arr = np.array([1, 2, 3, 4, 5, 6])
 print(np.split(arr, 2))
+
+#unequally split
+arr = np.array([1, 2, 3, 4, 5, 6])
+print(np.split(arr, [1, 3])) # splits at 1 then 2 and then 3
+
+
