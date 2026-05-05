e0395a65b68f1b8985bffe719d0257a2861b27e6
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-05-04 20:17:07 -0500

    normal for loops take time

--- broadcasting.py
+++ broadcasting.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+# first with loops
+
+prices = [10, 20, 30, 40, 50]
+discount = 10
+fianl_prices = []
+for price in prices:
+    final_price = price - (price * discount / 100)
+    fianl_prices.append(final_price)
+print(fianl_prices)
