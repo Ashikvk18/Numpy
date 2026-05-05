b667d17f1041c5f607d7ceaaaba6de496e288efe
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-05-04 20:31:59 -0500

    fast

--- fast.py
+++ fast.py
@@ -2,4 +2,8 @@ import numpy as np
 arr1 = np.array([1,2,3,4,5])
 arr2 = np.array([6,7,8,9,10])
 
-print(arr1 + arr2)
+multiplied1 = arr1 * 7
+print(multiplied1)
+
+multiplied2 = arr2 * 7
+print(multiplied2)
