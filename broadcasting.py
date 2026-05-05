d54fc09793e42d6bd0fdb1087c9001812a0c713a
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-05-04 20:17:57 -0500

    broadcasting and for loop difference

--- broadcasting.py
+++ broadcasting.py
@@ -9,3 +9,12 @@ for price in prices:
     final_price = price - (price * discount / 100)
     fianl_prices.append(final_price)
 print(fianl_prices)
+
+
+# now with numpy
+
+prices = np.array([10, 20, 30, 40, 50])
+discount = 10
+fianl_prices = prices - (prices * discount / 100)
+print(fianl_prices)
+
